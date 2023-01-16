da70fb7f160e35843b6b3f9b3ed393d731e436c3
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-01-16 14:50:32 +1100

    added contact details to footers

--- a2/booking.php
+++ a2/booking.php
@@ -28,9 +28,18 @@
       </article>
     </main>
     <footer>
+      <div class="contactDetails">
+        <em>Contact Us:</em>
+        email - <a href="mailto:info@lunardocineamas.com.au" target="_blank" >info@lunardocineamas.com.au</a> 
+        Mobile - <a href="tel:0439 555 5555" target="_blank">Call 0439 555 5555</a>
+        Address - <a href="https://www.openstreetmap.org/node/4301500443#map=19/-34.06668/150.81231" target="_blank">
+          1 Cinema Rd, Movie Town, NSW 2999
+        </a>
+      </div>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script> Matthew Rohrich, s3256609. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      <a id="github" href="https://github.com/matthewrohrich/wp" target="_blank">Matthews Github</a> &nbsp;
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
