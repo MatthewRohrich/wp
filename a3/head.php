40c50432a8ec04392f33c6815d70f99095a08fc9
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-02-18 10:26:05 +1100

    header and footer moved into modules

---
+++ a3/head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lunardo Cinemas</title>
+
+    <!-- Keep wireframe.css for debugging, add your css to style.css -->
+    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
+    <script src='../wireframe.js'></script>
+    <script src='./script.js'></script>
+
+</head>
+
+<body>
+
+    <header>
+        <!-- https://pixabay.com/vectors/film-cinema-popcorn-coke-fun-162029/ -->
+        <div id="cinemaTitle"><img id='logo' src='../../media/logo-160.png' alt='Film and popcorn'> Lunardo Cinemas
+        </div>
+    </header>
+
+    <!-- photos -->
+    <!-- https://www.imdb.com/name/nm0757855/mediaviewer/rm4148826625?ref_=nmmi_mi_all_sf_7   zoe saldana -->
+    <!-- https://www.imdb.com/name/nm0941777/mediaviewer/rm3947500033?ref_=nmmi_mi_all_sf_14 Sam Worthington -->
+    <!--   James Cameron -->
+    <!-- https://archive.org/download/bliptv-20131031-094703-Bsgcast-AVATARPREVIEWExclusiveScenesAndInterviews149  avatar preview -->
---
+++ a3/receipt.php
@@ -0,0 +1,10 @@
+<?php include './head.php' ?>
+
+<h1>receipt goes here</h1>
+
+
+</body>
+<?php include './footer.php' ?>
+<?php include './debug.php' ?>
+
+</html>
