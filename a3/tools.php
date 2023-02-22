927125d6e5184943cfbbcb8cda475c5226be0f19
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-02-22 12:02:38 +1100

    seats array created

--- a3/tools.php
+++ a3/tools.php
@@ -148,7 +148,39 @@ $movies = [
 ];
 
 
+$seats = [
+  'STA' => [
+    'Type' => 'Standard Adult',
+    'Discount' => '16.00',
+    'Normal' => '21.50'
+  ],
+  'STP' => [
+    'Type' => 'Standard Concession',
+    'Discount' => '14.50',
+    'Normal' => '19.00'
+  ],
+  'STC' => [
+    'Type' => 'Standard Child',
+    'Discount' => '13.00',
+    'Normal' => '17.50'
+  ],
+  'FCA' => [
+    'Type' => 'First Class Adult',
+    'Discount' => '25.00',
+    'Normal' => '31.00'
+  ],
+  'FCP' => [
+    'Type' => 'First Class Concession',
+    'Discount' => '23.50',
+    'Normal' => '28.00'
+  ],
+  'FCC' => [
+    'Type' => 'First Class Child',
+    'Discount' => '22.00',
+    'Normal' => '25.00'
+  ],
 
+];
 
 function showAllMovies()
 {
