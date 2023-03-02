33cf7abe2a1edec3e6f5f56c306baba570d1135e
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-03-02 16:29:19 +1100

    working checkbox to remember/forget

--- a4/booking.php
+++ a4/booking.php
@@ -1,190 +1,206 @@
-<?php 
-  session_start();
-  $moviesObject = [
-    'ACT' => [
-      'title' => 'Avatar: The Way of Water',
-      'rating' => 'M',
-      'genre' => 'Action, Adventure, Sci-Fi',
-      'plot' => 'Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Navi race to protect their home.',
-      'imdb' => 'tt1630029',
-      'screening-summary' => 'Mon-Tue: 9pm, Wed-Fri: 9pm, Sat-Sun: 6pm',
-      'screenings' => [
-        'MON' => [
-          'time' => '9pm',
-          'rate' => 'discount'
-        ],
-        'TUE' => [
-          'time' => '9pm',
-          'rate' => 'regular'
-        ],
-        'WED' => [
-          'time' => '9pm',
-          'rate' => 'regular'
-        ],
-        'THU' => [
-          'time' => '9pm',
-          'rate' => 'regular'
-        ],
-        'FRI' => [
-          'time' => '9pm',
-          'rate' => 'regular'
-        ],
-        'SAT' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ],
-        'SUN' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ]
-      ]
-    ], 
-    'RMC' => [
-      'title' => 'Weird: The Al Yankovic Story',
-      'rating' => 'M',
-      'genre' => 'Romance, Drama, Comedy',
-      'plot' => 'Explores every facet of Yankovics life, from his meteoric rise to fame with early hits like Eat It and Like a Surgeon to his torrid celebrity love affairs and famously depraved lifestyle.',
-      'imdb' => 'tt17076046',
-      'screening-summary' => 'Mon-Tue: Not Showing, Wed-Fri: 12pm, Sat-Sun: 3pm',
-      'screenings' => [
-        'WED' => [
-          'time' => '12pm',
-          'rate' => 'discount'
-        ],
-        'THU' => [
-          'time' => '12pm',
-          'rate' => 'discount'
-        ],
-        'FRI' => [
-          'time' => '12pm',
-          'rate' => 'discount'
-        ],
-        'SAT' => [
-          'time' => '3pm',
-          'rate' => 'regular'
-        ],
-        'SUN' => [
-          'time' => '3pm',
-          'rate' => 'regular'
-        ]
-      ]
-    ],
-    'FAM' => [
-      'title' => 'Puss in Boots: The Last Wish',
-      'rating' => 'PG-13',
-      'genre' => 'Animation, Action, Adventure',
-      'plot' => 'When Puss in Boots discovers that his passion for adventure has taken its toll and he has burned through eight of his nine lives, he launches an epic journey to restore them by finding the mythical Last Wish.',
-      'imdb' => 'tt3915174',
-      'screening-summary' => 'Mon-Tue: 12pm, Wed-Fri: 6pm, Sat-Sun: 12pm',
-      'screenings' => [
-        'MON' => [
-          'time' => '12pm',
-          'rate' => 'discount'
-        ],
-        'TUE' => [
-          'time' => '12pm',
-          'rate' => 'discount'
-        ],
-        'WED' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ],
-        'THU' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ],
-        'FRI' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ],
-        'SAT' => [
-          'time' => '12pm',
-          'rate' => 'regular'
-        ],
-        'SUN' => [
-          'time' => '12pm',
-          'rate' => 'regular'
-        ]
-      ]
-    ],
-    'AHF' => [
-      'title' => 'Margrete: Queen of the North',
-      'rating' => 'MA15+',
-      'genre' => 'Action, Drama, History',
-      'plot' => '1402. Queen Margrete is ruling Sweden, Norway and Denmark through her adopted son, Erik. But a conspiracy is in the making and Margrete finds herself in an impossible dilemma that could shatter her lifes work: the Kalmar Union.',
-      'imdb' => 'tt9308390',
-      'screening-summary' => 'Mon-Tue: 6pm, Wed-Fri: Not Showing, Sat-Sun: 10pm',
-      'screenings' => [
-        'MON' => [
-          'time' => '6pm',
-          'rate' => 'discount'
-        ],
-        'TUE' => [
-          'time' => '6pm',
-          'rate' => 'regular'
-        ],
-        'SAT' => [
-          'time' => '10pm',
-          'rate' => 'regular'
-        ],
-        'SUN' => [
-          'time' => '10pm',
-          'rate' => 'regular'
-        ]
+<?php
+session_start();
+$moviesObject = [
+  'ACT' => [
+    'title' => 'Avatar: The Way of Water',
+    'rating' => 'M',
+    'genre' => 'Action, Adventure, Sci-Fi',
+    'plot' => 'Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Navi race to protect their home.',
+    'imdb' => 'tt1630029',
+    'screening-summary' => 'Mon-Tue: 9pm, Wed-Fri: 9pm, Sat-Sun: 6pm',
+    'screenings' => [
+      'MON' => [
+        'time' => '9pm',
+        'rate' => 'discount'
+      ],
+      'TUE' => [
+        'time' => '9pm',
+        'rate' => 'regular'
+      ],
+      'WED' => [
+        'time' => '9pm',
+        'rate' => 'regular'
+      ],
+      'THU' => [
+        'time' => '9pm',
+        'rate' => 'regular'
+      ],
+      'FRI' => [
+        'time' => '9pm',
+        'rate' => 'regular'
+      ],
+      'SAT' => [
+        'time' => '6pm',
+        'rate' => 'regular'
+      ],
+      'SUN' => [
+        'time' => '6pm',
+        'rate' => 'regular'
       ]
     ]
-  ];
-
-  $seatsObject = [
-    'STA'=> [
-      'desc' => 'Standard Adult',
-      'regular' => 21.50,
-      'discount' => 16.00
-    ],
-    'STP'=> [
-      'desc' => 'Standard Concession',
-      'regular' => 19.00,
-      'discount' => 14.50
-    ],
-    'STC'=> [
-      'desc' => 'Standard Child',
-      'regular' => 17.50,
-      'discount' => 13.00
-    ],
-    'FCA'=> [
-      'desc' => 'First Class Adult',
-      'regular' => 31.00,
-      'discount' => 25.00
-    ],
-    'FCP'=> [
-      'desc' => 'First Class Concession',
-      'regular' => 28.00,
-      'discount' => 23.50
-    ],
-    'FCC'=> [
-      'desc' => 'First Class Child',
-      'regular' => 25.00,
-      'discount' => 22.00
+  ],
+  'RMC' => [
+    'title' => 'Weird: The Al Yankovic Story',
+    'rating' => 'M',
+    'genre' => 'Romance, Drama, Comedy',
+    'plot' => 'Explores every facet of Yankovics life, from his meteoric rise to fame with early hits like Eat It and Like a Surgeon to his torrid celebrity love affairs and famously depraved lifestyle.',
+    'imdb' => 'tt17076046',
+    'screening-summary' => 'Mon-Tue: Not Showing, Wed-Fri: 12pm, Sat-Sun: 3pm',
+    'screenings' => [
+      'WED' => [
+        'time' => '12pm',
+        'rate' => 'discount'
+      ],
+      'THU' => [
+        'time' => '12pm',
+        'rate' => 'discount'
+      ],
+      'FRI' => [
+        'time' => '12pm',
+        'rate' => 'discount'
+      ],
+      'SAT' => [
+        'time' => '3pm',
+        'rate' => 'regular'
+      ],
+      'SUN' => [
+        'time' => '3pm',
+        'rate' => 'regular'
+      ]
     ]
-  ];
+  ],
+  'FAM' => [
+    'title' => 'Puss in Boots: The Last Wish',
+    'rating' => 'PG-13',
+    'genre' => 'Animation, Action, Adventure',
+    'plot' => 'When Puss in Boots discovers that his passion for adventure has taken its toll and he has burned through eight of his nine lives, he launches an epic journey to restore them by finding the mythical Last Wish.',
+    'imdb' => 'tt3915174',
+    'screening-summary' => 'Mon-Tue: 12pm, Wed-Fri: 6pm, Sat-Sun: 12pm',
+    'screenings' => [
+      'MON' => [
+        'time' => '12pm',
+        'rate' => 'discount'
+      ],
+      'TUE' => [
+        'time' => '12pm',
+        'rate' => 'discount'
+      ],
+      'WED' => [
+        'time' => '6pm',
+        'rate' => 'regular'
+      ],
+      'THU' => [
+        'time' => '6pm',
+        'rate' => 'regular'
+      ],
+      'FRI' => [
+        'time' => '6pm',
+        'rate' => 'regular'
+      ],
+      'SAT' => [
+        'time' => '12pm',
+        'rate' => 'regular'
+      ],
+      'SUN' => [
+        'time' => '12pm',
+        'rate' => 'regular'
+      ]
+    ]
+  ],
+  'AHF' => [
+    'title' => 'Margrete: Queen of the North',
+    'rating' => 'MA15+',
+    'genre' => 'Action, Drama, History',
+    'plot' => '1402. Queen Margrete is ruling Sweden, Norway and Denmark through her adopted son, Erik. But a conspiracy is in the making and Margrete finds herself in an impossible dilemma that could shatter her lifes work: the Kalmar Union.',
+    'imdb' => 'tt9308390',
+    'screening-summary' => 'Mon-Tue: 6pm, Wed-Fri: Not Showing, Sat-Sun: 10pm',
+    'screenings' => [
+      'MON' => [
+        'time' => '6pm',
+        'rate' => 'discount'
+      ],
+      'TUE' => [
+        'time' => '6pm',
+        'rate' => 'regular'
+      ],
+      'SAT' => [
+        'time' => '10pm',
+        'rate' => 'regular'
+      ],
+      'SUN' => [
+        'time' => '10pm',
+        'rate' => 'regular'
+      ]
+    ]
+  ]
+];
 
-  function Daymyname($DAY) {
-    switch ($DAY) {
-      case 'MON': return 'Monday';    break;
-      case 'TUE': return 'Tuesday';   break;
-      case 'WED': return 'Wednesday'; break;
-      case 'THU': return 'Thursday';  break;
-      case 'FRI': return 'Friday';    break;
-      case 'SAT': return 'Saturday';  break;
-      case 'SUN': return 'Sunday';    break;
-      default: return "Funday";
-    }
-  }
+$seatsObject = [
+  'STA' => [
+    'desc' => 'Standard Adult',
+    'regular' => 21.50,
+    'discount' => 16.00
+  ],
+  'STP' => [
+    'desc' => 'Standard Concession',
+    'regular' => 19.00,
+    'discount' => 14.50
+  ],
+  'STC' => [
+    'desc' => 'Standard Child',
+    'regular' => 17.50,
+    'discount' => 13.00
+  ],
+  'FCA' => [
+    'desc' => 'First Class Adult',
+    'regular' => 31.00,
+    'discount' => 25.00
+  ],
+  'FCP' => [
+    'desc' => 'First Class Concession',
+    'regular' => 28.00,
+    'discount' => 23.50
+  ],
+  'FCC' => [
+    'desc' => 'First Class Child',
+    'regular' => 25.00,
+    'discount' => 22.00
+  ]
+];
 
-  $movie = 'ACT'; // default movie setting for A4 simplicity
-  if ( isset($_GET['movie']) && isset($moviesObject[$_GET['movie']]) ) {
-    $movie = $_GET['movie'];
+function Daymyname($DAY)
+{
+  switch ($DAY) {
+    case 'MON':
+      return 'Monday';
+      break;
+    case 'TUE':
+      return 'Tuesday';
+      break;
+    case 'WED':
+      return 'Wednesday';
+      break;
+    case 'THU':
+      return 'Thursday';
+      break;
+    case 'FRI':
+      return 'Friday';
+      break;
+    case 'SAT':
+      return 'Saturday';
+      break;
+    case 'SUN':
+      return 'Sunday';
+      break;
+    default:
+      return "Funday";
   }
+}
+
+$movie = 'ACT'; // default movie setting for A4 simplicity
+if (isset($_GET['movie']) && isset($moviesObject[$_GET['movie']])) {
+  $movie = $_GET['movie'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -194,18 +210,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Lunardo Booking Page</title>
+
   <style>
     body {
       font-family: Arial;
     }
+
     fieldset {
       padding: 10px;
       margin-bottom: 20px;
     }
-    button, input, select {
+
+    button,
+    input,
+    select {
       padding: 10px;
       margin: 5px;
     }
+
     label {
       display: inline-flex;
       align-items: center;
@@ -213,9 +235,13 @@
       border-radius: 10px;
       padding: 5px 10px 5px 5px;
       margin: 10px;
-      
+
     }
   </style>
+
+  <!-- <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>"> -->
+
+  <script src='./script.js'></script>
   <script>
     var moviesObjectJS = <?= json_encode($moviesObject, JSON_PRETTY_PRINT) ?>;
     var seatsObjectJS = <?= json_encode($seatsObject, JSON_PRETTY_PRINT) ?>;
@@ -238,12 +264,23 @@
 
     <h2>Movie Selection</h2>
 
-    <p>Movie Title: <?= $moviesObject[$movie]['title'] ?></p>
-    <p>Rating: <?= $moviesObject[$movie]['rating'] ?></p>
-    <p>Genre: <?= $moviesObject[$movie]['genre'] ?></p>
-    <p>Plot: <?= $moviesObject[$movie]['plot'] ?></p>
-    <p>IMDB Link: <a href='https://www.imdb.com/title/<?= $moviesObject[$movie]['imdb'] ?>/' target="_blank">More about <?= $moviesObject[$movie]['title'] ?> over at IMDB</a></p>
-    <p>Screening Times: <?= $moviesObject[$movie]['screening-summary'] ?></p>
+    <p>Movie Title:
+      <?= $moviesObject[$movie]['title'] ?>
+    </p>
+    <p>Rating:
+      <?= $moviesObject[$movie]['rating'] ?>
+    </p>
+    <p>Genre:
+      <?= $moviesObject[$movie]['genre'] ?>
+    </p>
+    <p>Plot:
+      <?= $moviesObject[$movie]['plot'] ?>
+    </p>
+    <p>IMDB Link: <a href='https://www.imdb.com/title/<?= $moviesObject[$movie]['imdb'] ?>/' target="_blank">More about
+        <?= $moviesObject[$movie]['title'] ?> over at IMDB</a></p>
+    <p>Screening Times:
+      <?= $moviesObject[$movie]['screening-summary'] ?>
+    </p>
 
     <hr>
 
@@ -256,25 +293,25 @@
       <fieldset id="screening-set">
         <legend>Screenings</legend>
         <?php
-          foreach($moviesObject[$movie]['screenings'] as $day => $details) {
-            echo "<label><input type='radio' id='$day' name='day' value='$day' />" . 
-            Daymyname($day) . 
+        foreach ($moviesObject[$movie]['screenings'] as $day => $details) {
+          echo "<label><input type='radio' id='$day' name='day' value='$day' />" .
+            Daymyname($day) .
             " {$details['time']} ({$details['rate']})</label>";
-          }
+        }
         ?>
       </fieldset>
 
       <fieldset id="seating-set">
         <legend>Seating</legend>
         <?php
-          foreach($seatsObject as $seat => $details) {
-            echo "<label>$seat</label> <select name='seats[$seat]'>\n";
-            echo "  <option value=\"\">Please Select</option>\n";
-            for ($s=1; $s<=10; $s++) {
-              echo "  <option value=\"$s\">$s</option>\n";
-            }
-            echo "</select><br>";
+        foreach ($seatsObject as $seat => $details) {
+          echo "<label>$seat</label> <select name='seats[$seat]'>\n";
+          echo "  <option value=\"\">Please Select</option>\n";
+          for ($s = 1; $s <= 10; $s++) {
+            echo "  <option value=\"$s\">$s</option>\n";
           }
+          echo "</select><br>";
+        }
         ?>
       </fieldset>
 
@@ -285,6 +322,70 @@
         <input type='tel' name="user[mobile]" id="mobile" value='' placeholder="Mobile" />
       </fieldset>
 
+      <fieldset>
+        <div class='button'>
+          <input type='checkbox' id='remember' name='remember'>
+          <label id='remember-label' for='checkbox'>Remember Me</label>
+        </div>
+      </fieldset>
+      <button id='showme'>Show me the stores</button>
+
+      <script type="text/javascript">
+        var store = window.localStorage;
+
+        window.onload = function () {
+          //store.clear();
+          if (store.getItem('name'))
+          {
+            document.getElementById('remember').checked = true;
+            document.getElementById('remember-label').textContent = 'Forget Me';
+            document.getElementById('name').value = store.getItem('name');
+            document.getElementById('email').value = store.getItem('email');
+            document.getElementById('mobile').value = store.getItem('mobile');
+          }
+          else
+          {
+            document.getElementById('remember-label').textContent = 'Remember Me';
+
+          }
+        };
+
+        // var outputElement = document.getElementById('showresult');
+
+        // // probably won't need this.
+        // function replace(element, text) {
+        //   element.textContent = text;
+
+        // };
+
+        document.getElementById('remember').addEventListener('click', function () {
+          if (this.checked)
+          {
+            //needs validation of valid inputs
+            store.setItem('name', document.getElementById('name').value);
+            store.setItem('email', document.getElementById('email').value);
+            store.setItem('mobile', document.getElementById('mobile').value);
+            document.getElementById('showresult').textContent = 'created the results';
+          } else
+          {
+            store.clear();
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('mobile').value = '';
+            document.getElementById('showresult').textContent = 'deleted the results';
+          }
+
+
+        });
+
+        document.getElementById('showme').addEventListener('click', function () {
+          var result;
+          result = store.getItem('name') + ' ' + store.getItem('email') + ' ' + store.getItem('mobile');
+          document.getElementById('showresult').textContent = result;
+          //replace(outputElement, result);
+        });
+      </script>
+      <span id='showresult'></span>
       <fieldset>
         <legend>Submit Options</legend>
         <button id="submitCS" name='submitCS'>Order &amp; check<br>Client-Side Validation</button>
@@ -299,18 +400,19 @@
   <footer>
     Assignment 4 Fallback Template Debug Area
     <pre>
-$movie <?= $movie ?>
+      $movie
+      <?= $movie ?>
 
-seatsObject contains:
-<?php print_r($seatsObject); ?>
-moviesObject contains:
-<?php print_r($moviesObject); ?>
-GET contains:
-<?php print_r($_GET); ?>
-POST contains:
-<?php print_r($_POST); ?>
-SESSION contains:
-<?php print_r($_SESSION); ?>
+      seatsObject contains:
+      <?php print_r($seatsObject); ?>
+      moviesObject contains:
+      <?php print_r($moviesObject); ?>
+      GET contains:
+      <?php print_r($_GET); ?>
+      POST contains:
+      <?php print_r($_POST); ?>
+      SESSION contains:
+      <?php print_r($_SESSION); ?>
     </pre>
   </footer>
 </body>
