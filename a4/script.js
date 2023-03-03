45ddaed7df2d0fe5ce2b3c5da1c9046f5fc6220d
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-03-03 14:21:48 +1100

    moved javescript for remember me to script.js

--- a4/script.js
+++ a4/script.js
@@ -27,3 +27,48 @@ window.onscroll = function () {
     }
   }
 };
+
+// remembering the user details using local storage
+var store = window.localStorage;
+
+// as soon as the page is finished rendering fill the user details form if they exist in local storage
+window.onload = function () {
+  //store.clear();
+  if (store.getItem("name")) {
+    document.getElementById("remember").checked = true;
+    document.getElementById("remember-label").textContent = "Forget Me";
+    document.getElementById("name").value = store.getItem("name");
+    document.getElementById("email").value = store.getItem("email");
+    document.getElementById("mobile").value = store.getItem("mobile");
+  } else {
+    document.getElementById("remember-label").textContent = "Remember Me";
+  }
+};
+
+//save the user details from the form to local storage
+// acts like a toggle on press remembers next press deletes
+document.getElementById("remember").addEventListener("click", function () {
+  if (this.checked) {
+    //needs validation of valid inputs
+    store.setItem("name", document.getElementById("name").value);
+    store.setItem("email", document.getElementById("email").value);
+    store.setItem("mobile", document.getElementById("mobile").value);
+    document.getElementById("remember-label").textContent = "Forget Me";
+    //document.getElementById('remember-errors').textContent = 'created the results';
+  } else {
+    store.clear();
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("mobile").value = "";
+    document.getElementById("remember-label").textContent = "Remember Me";
+    //document.getElementById('remember-errors').textContent = 'deleted the results';
+  }
+});
+
+//button for testing
+// document.getElementById('showme').addEventListener('click', function () {
+//   var result;
+//   result = store.getItem('name') + ' ' + store.getItem('email') + ' ' + store.getItem('mobile');
+//   document.getElementById('remember-errors').textContent = result;
+//   //replace(outputElement, result);
+// });
