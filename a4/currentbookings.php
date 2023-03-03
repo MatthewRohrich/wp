2a8cb6940557d28f6f620186dc6010898e0ede45
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2023-03-03 20:57:38 +1100

    pseudo code plan for current bookings

--- a4/currentbookings.php
+++ a4/currentbookings.php
@@ -1,9 +1,20 @@
 <?php
 include './tools.php';
 include './head.php';
+
+// Assumptions
+// data stored in bookings.txt
+
 ?>
 
 
+
+<!-- open the bookings.txt file put in an Array assume max 50 bookings -->
+<!--  -->
+<!-- find any entries matching name and email  -->
+<!-- display if found -->
+<!-- else show message "no bookings found" -->
+<!-- show a button that will send the information to the reciept page so the customer can print it. -->
 
 
 <?php include './footer.php' ?>
